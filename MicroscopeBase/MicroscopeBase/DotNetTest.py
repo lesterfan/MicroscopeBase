bf037bfc802505b7ace97ec99d6eaeb5c2f4b422
Lester Fan <hf155@scarletmail.rutgers.edu>
2016-08-09 16:29:21 -0400

    Ported the C# script onto Python

--- MicroscopeBase/MicroscopeBase/DotNetTest.py
+++ MicroscopeBase/MicroscopeBase/DotNetTest.py
@@ -1,25 +1,66 @@
 import dotnet.seamless
 
+# Load the compiled C# library... see the C# library to see how everything works
 dotnet.add_assemblies('C:\\Users\\Huafeng\\Desktop\\VGithub\\MicroscopeBase\\MicroscopeBase\\MicroscopeBase\\')
-dotnet.print_pretty_names(dotnet.assemblies())
-
-# Load the assembly
-dotnet.load_assemblies('FIRemoteCOM')
-# 
-# 
-dotnet.print_pretty_names(dotnet.namespaces("FIRemoteCOM"))
-# 
-# dotnet.build_assembly(FilmmetricsAnalysisString, 'FilmmetricsAnalysis.dll', ['FILMeasure.exe', 'System.Xml.dll', 'System.Drawing.dll'], '/debug')
-# 
-import FIRemoteCOM
+dotnet.load_assembly('MicroscopeAnalyzerLibrary')
+import MicroscopeAnalyzerLibrary
 
 
 def main():
-    # mAnalyzer = FilmetricsAnalysis.MicroscopeAnalyzer(True)
+    mAnalyzer = MicroscopeAnalyzerLibrary.MicroscopeAnalyzer(True)
+    if mAnalyzer.mLastRet == 1:
+        print "Something wrong in the last step!"
 
-    mFIRemote = FIRemoteCOM.FIRemote(True)
+    while True:
+        input = raw_input( "Enter in 'a' to take measurement, enter in 's' to save, 'l' to load\n" )
 
+        if input == 'a':
+            print "Now measuring!"
 
-    print "Hello, world!"
+            # Emulates click on the 'measure' button
+            mAnalyzer.Measure()
+            if mAnalyzer.mLastRet == 1:
+                print "Something wrong in the last step!"
+
+            print "Measurement successful!"
+
+        elif input == 's':
+            mAnalyzer.mMeasuredResults.ret = "Saved!"
+    
+            print "Please enter in the name of the file you want it to be saved in."
+            print "The program will automatically save it as a '.fmspe' file. "
+            print "For now, all files will be saved in directory 'C:/Users/HMNL/Documents/Test/' "
+
+            currFileDir = "C:/Users/HMNL/Documents/Test/"
+            user_input = raw_input("\n")
+
+            # Save the .fmspe file
+            mAnalyzer.SaveSpectrum(currFileDir, user_input)
+            if mAnalyzer.mLastRet == 1:
+                print "Something wrong in the last step!"
+
+            # Save the .xml data structure and the image.
+            mAnalyzer.SaveResultsTo(currFileDir, user_input)
+            if mAnalyzer.mLastRet == 1:
+                print "Something wrong in the last step!"
+
+        elif input == 'l':
+            print "Please enter in the name of the file you want the settings loaded from."
+            print "For now, all files will be saved from directory 'C:/Users/HMNL/Documents/Test/' "
+            
+            currFileDir = "C:/Users/HMNL/Documents/Test/"
+            user_input = raw_input("\n")
+
+            # Load the result
+            loaded_result = MicroscopeAnalyzerLibrary.MicroscopeAnalyzer.LoadResultsFrom(currFileDir, user_input)
+            if loaded_result == None:
+                print "Something wrong in the last step"
+                continue
+
+            print "Load successful!"
+            for item in loaded_result.PrimaryWavelengths:
+                print item
+
+            print "The summary is ", loaded_result.ResultsSummary
 
 main()
