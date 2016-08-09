82ac7b87323acd347559fc2b69523ef838cd0bbe
Lester Fan <hf155@scarletmail.rutgers.edu>
2016-08-09 18:00:09 -0400

    Further code clean-up

--- MicroscopeBase/MicroscopeBase/UserInterface.py
+++ MicroscopeBase/MicroscopeBase/UserInterface.py
@@ -22,9 +22,19 @@ class UserInterface:
     start_button = 0                          # Internal Pygame variable
     rt_button = 0                             # Internal Pygame variable
 
+    a_button_num = 0                          # Which joystick numbers should be checked internally
+    x_button_num = 0                          # Which joystick numbers should be checked internally
+    y_button_num = 0                          # Which joystick numbers should be checked internally
+    b_button_num = 0                          # Which joystick numbers should be checked internally
+    rb_button_num = 0                         # Which joystick numbers should be checked internally
+    start_button_num = 0                      # Which joystick numbers should be checked internally
+    rt_button_num = 0                         # Which joystick numbers should be checked internally
+
+
     display_width = 320                       # Internal Pygame variable
     display_height = 484                      # Internal Pygame variable
    
+    pygame_display = None                     # Reference to pygame display
 
     using_joystick = False                    # Will automatically be set based on if a joystick is connected when the program starts
     joystick = None                           # Reference to joystick object
@@ -37,18 +47,100 @@ class UserInterface:
 
     message1 = ""                             # Message to show the user
     
+
+    ''' 
+    Constructor that sets up connection to user interface. Instantiates all the 
+    internal variables
+    @ param pygame_title : String that shows the text that should be displayed
+    Returns None
+    '''
     def __init__(self, pygame_title = "HMNL (c) 2016: All rights reserved."):
 
         # Initialize pygame / joystick
-        pygame.init()                        
+        pygame.init()
+
+        # Setting up pygame internal variables
+        self.FPS = 60
+        self.Clock = pygame.time.Clock()
+        self.pygame_display = pygame.display.set_mode((self.display_width, self.display_height))
+        pygame.display.set_caption(pygame_title)
+
+
+    '''
+    Initializes the self.joystick variable and maps the respective button nums to their rightful
+    positions 
+    '''
+    def initialize_joystick(self, a_button_num, x_button_num, y_button_num,
+                            b_button_num, rb_button_num, start_button_num, rt_button_num):
+                                
         pygame.joystick.init()
 
         if pygame.joystick.get_count() != 0:
-
+            print "Using joystick!"
             self.using_joystick = True
 
+            # Setting up the internal joystick nums
+            self.a_button_num         =  a_button_num    
+            self.x_button_num         =  x_button_num    
+            self.y_button_num         =  y_button_num    
+            self.b_button_num         =  b_button_num    
+            self.rb_button_num        =  rb_button_num   
+            self.start_button_num     =  start_button_num 
+            self.rt_button_num        =  rt_button_num   
 
-    def initialize_joystick(self, joystick_num = 0):
+            # Initialize the joystick
+            self.joystick = pygame.joystick.Joystick(0)
+            self.joystick.init()
 
-        joystick = pygame.joystick.Joystick(joystick_num)
+        else:
+            print "Not using joystick!"
+            
 
+    '''
+    Refreshes the display after each frame.
+    Call on each loop.
+    '''
+    def refresh_display(self):
+        pygame.display.update()
+        self.Clock.tick(self.FPS)
+
+    ''' 
+    Reads whatever key is being inputted. Call before running any of the checker functions
+    '''
+    def get_keys(self):
+        self.keys = pygame.key.get_pressed()
+
+    
+    '''
+    The next few functions detect specific keyboard inputs described by their name
+    '''
+
+    def left_key_pressed(self):
+        if self.keys[pygame.K_LEFT]:
+            return True
+        else:
+            return False
+
+    def right_key_pressed(self):
+        if self.keys[pygame.K_RIGHT]:
+            return True
+        else:
+            return False
+
+    def down_key_pressed(self):
+        if self.keys[pygame.K_DOWN]:
+            return True
+        else:
+            return False
+        
+    # def right_key_pressed(self):
+    #     if self.keys[pygame.K_UP]:
+    #         return True
+    #     else:
+    #         return False
+    #     
+    # def right_key_pressed(self):
+    #     if self.keys[pygame.K_RIGHT]:
+    #         return True
+    #     else:
+    #         return False
