09f04e95b61848296d868c701f5931d8c410f5ff
HMNL <HMNL@HMNL>
2016-08-29 11:02:59 -0400

    GUI works... now just add the post processing part

--- MicroscopeBase/MicroscopeBase/UserInterface.py
+++ MicroscopeBase/MicroscopeBase/UserInterface.py
@@ -377,9 +377,9 @@ class UserInterface:
 
 
         # Print messages to screen
-        printfunctions.message_to_screen("1 unit = 1 microstep = 0.15625 um", colors.red, y_displace = 110, x_displace = -165, size = 'small')
-        printfunctions.message_to_screen("Location : "+str(absolute_location), colors.black, y_displace = 125, x_displace = -165, size = 'medium')
-        printfunctions.message_to_screen(self.message1,colors.black, y_displace = 145, x_displace = -165)
+        printfunctions.message_to_screen("1 unit = 1 microstep = 0.15625 um", colors.red,    y_displace = 110, x_displace = -365, size = 'small')
+        printfunctions.message_to_screen("Location : "+str(absolute_location), colors.black, y_displace = 125, x_displace = -365, size = 'medium')
+        printfunctions.message_to_screen(self.message1,colors.black,                         y_displace = 145, x_displace = -365)
         
         pygame.display.update()
         self.Clock.tick(self.FPS)
